5127173e3c8ea86c1350ec4815a3b41ace64cd1e
wert777 <nickgryzenkov@yandex.ru>
2019-11-17 13:33:41 +1000

    wert 1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,18 @@
-print(1234567890)
+# Переменные
+a = 1
+b = a
+c, d = 5, 8
+print(a, b, c, d)
+a = 999
+print(a)
+a += 1
+print(a)
+a += 100
+print(a)
+# константы
+AAA = 700
+print("Вася", a)  #печатаем
+print(type(a))
+g = a + b
+print(g)
+print(a + b)
